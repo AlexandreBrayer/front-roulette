ba842f8b5ddab61b63672234055634765c0967aa
Alexandre <alexandre@jsc-trade?com>
2022-07-27 15:11:25 +0200

    [ADD] navigation for silvers

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,12 @@
   import Register from "./pages/Register.svelte";
   import Create from "./pages/Create.svelte";
   import Home from "./pages/Home.svelte";
+  import Silvers from "./pages/Silvers.svelte";
   export let url = "";
   import { token, user } from "./stores/store.js";
   import { onDestroy, onMount } from "svelte";
   let tokenValue;
+  let isSilver;
   function getUserInfo() {
     fetch(import.meta.env.VITE_API + "/login", {
       method: "GET",
@@ -29,13 +31,26 @@
     token.set(null);
     localStorage.removeItem("token"); 
   }
+  user.subscribe((value) => {
+    if (value) {
+      isSilver = value.silver;
+    }
+  });
   token.subscribe((value) => {
     tokenValue = value;
+  });
+  const userUnsubscribe = user.subscribe((value) => {
+    if (value) {
+      isSilver = value.silver;
+    }
   });
   const unsubscribe = token.subscribe((value) => {
     tokenValue = value;
   });
-  onDestroy(unsubscribe);
+  onDestroy(() => {
+    userUnsubscribe();
+    unsubscribe();
+  });
   onMount(async () => {
     if (tokenValue) {
       getUserInfo();
@@ -60,6 +75,9 @@
         {:else}
           <Link class="link" to="/">Home</Link>
           <Link class="link" to="/create">Create</Link>
+          {#if isSilver == true}
+            <Link class="link" to="/silvers">Silver</Link>
+          {/if}
           <button on:click={disconnect} class="button is-danger mt-1">Logout</button>
         {/if}
       </div>
@@ -71,5 +89,6 @@
     <Route path="/register"><Register /></Route>
     <Route path="/"><Home /></Route>
     <Route path="/create"><Create /></Route>
+    <Route path="/silvers"><Silvers /></Route>
   </div>
 </Router>
