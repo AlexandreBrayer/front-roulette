b998c40a49c084bd6dac6cda1b70452316d5f579
Alexandre <alexandre@jsc-trade?com>
2022-07-27 11:00:14 +0200

    [FIX] blank --> _blank

--- src/lib/Networks.svelte
+++ src/lib/Networks.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div class="networks">
-    <a href="https://github.com/AlexandreBrayer/front-roulette" target="blank" class="is-info mx-3">
+    <a href="https://github.com/AlexandreBrayer/front-roulette" target="_blank" class="is-info mx-3">
         <FontAwesomeIcon icon={faGithub} size="xl"/>
     </a>
-    <a href="https://trello.com/b/o6Whq9eb/strat-roulette" target="blank" class="is-info mx-3">
+    <a href="https://trello.com/b/o6Whq9eb/strat-roulette" target="_blank" class="is-info mx-3">
         <FontAwesomeIcon icon={faTrello} size="xl"/>
     </a>
 </div>
